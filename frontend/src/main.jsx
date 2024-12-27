dd631ecb7fb97b726308b6abaa16ca2da0d5ae15
Arnab Ghosh <arnab.ghosh.it2021@kgec.ac.in>
2024-12-27 12:20:04 +0530

    Enhance WebSocket functionality and improve code structure
    - Updated socket connection handling and event listeners in backend.
    - Refactored socket ID mapping for clarity.
    - Added new event listeners for call acceptance and negotiation in Room component.
    - Improved logging messages for better debugging.
    - Adjusted import statements for consistency in frontend components.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import { SocketProvider } from "./context/socketProvider.jsx";
+import { SocketProvider } from "./context/SocketProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
--- frontend/src/screens/Lobby.jsx
+++ frontend/src/screens/Lobby.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useCallback, useEffect } from "react";
-import { useSocket } from "../context/SocketProvider";
 import { useNavigate } from "react-router";
+import { useSocket } from "../context/SocketProvider";
 
 const LobbyScreen = () => {
   const [email, setEmail] = useState("");
   const [room, setRoom] = useState("");
+
   const socket = useSocket();
   const navigate = useNavigate();
 
